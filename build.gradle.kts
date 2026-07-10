de1f85e26b8e9e1db381ea3c34b0c7a342769a9e
Prashant Deshmukh <sived.official@gmail.com>
2026-07-10 14:49:50 +0530

    Add maven-publish plugin and jitpack.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library") version "8.2.2"
+    id("maven-publish")
 }
 
 android {
@@ -20,4 +21,14 @@ dependencies {
     implementation("com.google.android.gms:play-services-ads:23.0.0")
     implementation("com.google.android.gms:play-services-location:21.1.0")
     implementation("androidx.core:core:1.12.0")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
